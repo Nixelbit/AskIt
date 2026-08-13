6616e92360569b923711400ffa30ded0aea23739
nixelbit <nixelbit@gmail.com>
2026-08-13 18:13:19 +0300

    add sign up page

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_08_10_112123) do
+ActiveRecord::Schema[8.1].define(version: 2026_08_13_142131) do
   create_table "answers", force: :cascade do |t|
     t.text "body"
     t.datetime "created_at", null: false
@@ -24,6 +24,15 @@ ActiveRecord::Schema[8.1].define(version: 2026_08_10_112123) do
     t.datetime "created_at", null: false
     t.string "title"
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.string "email", null: false
+    t.string "name"
+    t.string "password_digest"
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
   add_foreign_key "answers", "questions"
